beb0b70136c834f964c4d24e01973b82176775e5
Rayfess <153087368+Rayfess@users.noreply.github.com>
2025-10-23 12:56:13 +0700

    refactor: variable.go adding const declare uses case;

--- variable.go
+++ variable.go
@@ -8,12 +8,14 @@ func main() {
 	name = "Jacky"
 	fmt.Println(name)
 
-
 	// Multiple declare variables
 	var (
 		firstName = "Junior"
 		lastName = "Downey"
 	)
-
 	fmt.Println(firstName, lastName)
+
+	// Declare using Const, cannot redeclare value
+	const age = 17
+	fmt.Println(age)
 }
